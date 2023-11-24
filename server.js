e8ab354eb1b8cab05123e6bf66b74a20cba97fb8
LoganLeach <122493729+LoganLeach12@users.noreply.github.com>
2023-11-24 15:01:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Update the connection URL based on your server's location
-    const socket = io('https://real-time-chatting.onrender.com');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
 
-    const messageContainer = document.getElementById('message-container');
-    const messageInput = document.getElementById('message-input');
-    const sendButton = document.getElementById('send-button');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-    sendButton.addEventListener('click', function (e) {
-      e.preventDefault();
-      sendMessage();
-    });
+app.use(express.static(__dirname));
+app.use(cors());
 
-    document.getElementById('send-container').addEventListener('submit', function (e) {
-      e.preventDefault();
-      sendMessage();
-    });
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
-    function sendMessage() {
-      const message = messageInput.value;
-      if (message.trim() !== '') {
-        socket.emit('chat message', message);
-        messageInput.value = '';
-      }
-    }
+  socket.broadcast.emit('user joined', 'User');
 
-    socket.on('chat message', function (msg) {
-      const messageElement = document.createElement('div');
-      messageElement.textContent = msg;
-      messageContainer.appendChild(messageElement);
-      messageContainer.scrollTop = messageContainer.scrollHeight;
-    });
-
-    socket.on('user joined', function (username) {
-      const joinMessage = document.createElement('div');
-      joinMessage.textContent = `${username} joined the chat`;
-      messageContainer.appendChild(joinMessage);
-      messageContainer.scrollTop = messageContainer.scrollHeight;
-    });
-
-    socket.on('user left', function (username) {
-      const leaveMessage = document.createElement('div');
-      leaveMessage.textContent = `${username} left the chat`;
-      messageContainer.appendChild(leaveMessage);
-      messageContainer.scrollTop = messageContainer.scrollHeight;
-    });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('user left', 'User');
+  });
+});
+
+// Update the PORT to the appropriate port number
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
